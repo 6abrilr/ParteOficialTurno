7f70e9b5396a5a5fe5d0111dc539a9470d2c75fd
nestor.g.rojas99 <nestor.g.rojas99@gmail.com>
2025-10-20 12:47:06 -0300

    Funciona 20oct25

--- php/auth/bootstrap.php
+++ php/auth/bootstrap.php
@@ -50,8 +50,8 @@ function url(string $path = ''): string {
 
 /** Escapar HTML (solo si no existe ya) */
 if (!function_exists('h')) {
-  function h($s): string {
-      return htmlspecialchars((string)$s, ENT_QUOTES, 'UTF-8');
+    function h(string|int|float|null $s): string {
+    return htmlspecialchars((string)$s, ENT_QUOTES, 'UTF-8');
   }
 }
 
